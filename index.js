048e34324e8c0d2ebb5ea4c14ff2d7f3e1d5126a
Dzuuw <heitorvieira40@gmail.com>
2024-07-12 11:38:27 -0300

    Commit 2

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+// código pro css funcionar no ejs
+app.use("/static", express.static(path.join(__dirname, "static")));
+app.set("view engine", "ejs");
+
 
 app.get("/", (req, res)=>{
-    res.send("Funcionando")
-})
+    res.render("main");
+});
 
 app.listen(6969, ()=>{
     console.log("Servidor rodando em http://localhost:6969")
